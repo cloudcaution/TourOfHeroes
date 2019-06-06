8fcaaa8b2ee1160db8fc2e42eab0eff36a85394b
yunzheng <ultrabushi@gmail.com>
2019-06-06 15:06:36 -0400

    add css to make UI look better

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Location } from '@angular/common';
+
 import { Hero } from '../hero';
+import { HeroService } from '../hero.service'; 
 
 @Component({
   selector: 'app-hero-detail',
@@ -9,10 +13,24 @@ import { Hero } from '../hero';
 export class HeroDetailComponent implements OnInit {
   // get input from parent component which is heroes
   // pass in input by given hero =>  [hero]="selectedHero"
-  @Input() hero: Hero;
-  constructor() { }
+  hero: Hero;
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location
+  ) { }
 
   ngOnInit() {
+    this.getHero();
+  }
+
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get('id');
+    this.heroService.getHero(id).subscribe(hero => this.hero = hero);
+  }
+
+  goBack(): void {
+    this.location.back();
   }
 
 }
